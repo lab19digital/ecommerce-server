200353cc0067df88c760605fe7d9efe2473f48b8
Alexeia <alexeiaw@gmail.com>
2019-12-16 14:18:59 +0200

    wip fixed pricing

--- src/Actions/CreateProduct.php
+++ src/Actions/CreateProduct.php
@@ -2,14 +2,15 @@
 
 namespace Lab19\Cart\Actions;
 
-use Lab19\Cart\Models\Product;
-use Lab19\Cart\Models\ProductAttribute;
 use Lab19\Cart\Actions\Helpers\Attributes;
+use Lab19\Cart\Factories\OpenExchangeRatesFactory;
 use Lab19\Cart\Models\Category;
+use Lab19\Cart\Models\Product;
+use Lab19\Cart\Models\ProductFixedPrice;
 
 class CreateProduct
 {
-    public static function handle( $args ): Product
+    public static function handle($args): Product
     {
         $product = new Product([
             'title' => $args['title'],
@@ -26,13 +27,13 @@ class CreateProduct
         $categories = $args['categories'] ?? [];
 
         $createCategories = [];
-        foreach( $categories as $category ){
-            if( isset($category['id']) ){
+        foreach ($categories as $category) {
+            if (isset($category['id'])) {
                 $cat = Category::find($category['id']);
-                if( $cat ){
-                    $product->categories()->attach( $cat );
+                if ($cat) {
+                    $product->categories()->attach($cat);
                 }
-            } else if(isset($category['title'])) {
+            } elseif (isset($category['title'])) {
                 $createCategories[] = [
                     'title' => $category['title']
                 ];
@@ -43,17 +44,33 @@ class CreateProduct
 
         $attributes = new Attributes();
         $attributes
-            ->meta( $args['meta'] ?? [] )
-            ->sizes( $args['sizes'] ?? [] )
-            ->dimensions( $args['dimensions'] ?? [])
-            ->weight( $args['weight'] ?? [] )
-            ->prices( $args['prices'] ?? [] );
+            ->meta($args['meta'] ?? [])
+            ->sizes($args['sizes'] ?? [])
+            ->dimensions($args['dimensions'] ?? [])
+            ->weight($args['weight'] ?? [])
+            ->prices($args['prices'] ?? []);
 
         $product->attributes()->createMany(
             $attributes->toArray()
         );
 
+        // Create fixed prices if specified in the arguments
+        $productPrice = $args['price_cents'] ?? false;
+        $productBaseCurrency = $args['price_currency'] ?? false;
+        $fixCurrencies = $args['fixprices'] ?? false;
+
+        if ($fixCurrencies && $productPrice && $productBaseCurrency) {
+            $convertedFixedPrices = array_map(function ($currencyCode) use ($productPrice, $productBaseCurrency) {
+                $converter = OpenExchangeRatesFactory::create($currencyCode, $productBaseCurrency);
+                $convertedPrice = $converter->convertCurrency($productPrice);
+
+                return new ProductFixedPrice(['country_code' => $currencyCode, 'price' => $convertedPrice]);
+            }, $fixCurrencies);
+
+            $product->fixedPrices()->saveMany($convertedFixedPrices);
+        }
+
+
         return $product;
     }
-
 }
