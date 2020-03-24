ac7d8a047d9f6347af3eccaf5a041b66294226e8
Alexeia <alexeiaw@gmail.com>
2020-03-24 12:39:46 +0200

    wip check if mapping exists

--- server/src/Packages/ExamplePackage/ExamplePackageProvider.php
+++ server/src/Packages/ExamplePackage/ExamplePackageProvider.php
@@ -4,6 +4,7 @@ namespace Gernzy\Server\Packages\ExamplePackage;
 
 use Gernzy\Server\Exceptions\GernzyException;
 use Gernzy\Server\Listeners\BeforeCheckout;
+use Gernzy\Server\Packages\ExamplePackage\Actions\ExampleBeforeCheckout;
 use Illuminate\Support\ServiceProvider;
 
 class ExamplePackageProvider extends ServiceProvider
@@ -30,9 +31,19 @@ class ExamplePackageProvider extends ServiceProvider
         // Example check if corresponding Event listener exists
         $events = config('events');
 
+        // Check if config has values and the appropriate Listener is present
         if (isset($events) && !array_key_exists(BeforeCheckout::class, $events) && !class_exists(BeforeCheckout::class)) {
             throw new GernzyException(
                 'The Event listener does not exist.',
+                'Please make sure the file exists in src/Listeners and the event is mapped in config/events.php.'
+            );
+        }
+
+        // Now check if correct action is mapped to the listener
+        $action = config('events.' . BeforeCheckout::class);
+        if (!isset($action) && $action != ExampleBeforeCheckout::class) {
+            throw new GernzyException(
+                'The Action does not exist.',
                 'Please make sure the file exists in src/Listeners and the event is mapped in config/events.php.'
             );
         }
