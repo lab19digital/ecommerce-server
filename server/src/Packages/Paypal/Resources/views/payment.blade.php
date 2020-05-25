da865748fe90417009924d3a0a32c4ff9984c6b6
Alexeia <alexeiaw@gmail.com>
2020-05-25 10:32:04 +0200

    feedback

--- server/src/Packages/Paypal/Resources/views/payment.blade.php
+++ server/src/Packages/Paypal/Resources/views/payment.blade.php
@@ -4,7 +4,7 @@
 <div class="uk-container uk-container-xsmall uk-margin-large-top">
     <h1 class="uk-heading-small uk-padding-small">Complete purchase</h1>
 </div>
-<div id="stripeFormTemplate" class="uk-container uk-container-xsmall uk-margin-large-top">
+<div id="paypalFormTemplate" class="uk-container uk-container-xsmall uk-margin-large-top">
     <div class="uk-card uk-card-default uk-card-body uk-width-auto uk-card-hover">
         <div>
             <div id="paypal-button-container"></div>
