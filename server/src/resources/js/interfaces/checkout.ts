764df47488ec65d5728951eba7a34aa0f992ab44
Alexeia <alexeiaw@gmail.com>
2020-05-27 11:48:28 +0200

    add populatePaymentProvider function to interface

--- server/src/resources/js/interfaces/checkout.ts
+++ server/src/resources/js/interfaces/checkout.ts
@@ -4,4 +4,5 @@ export interface GernzyCheckout {
     sendOfCheckoutInfo(values): Promise<any>;
     getBasketTotal(): Promise<any>;
     displayLineItems(): void;
+    populatePaymentProviders(): Promise<any>;
 }
