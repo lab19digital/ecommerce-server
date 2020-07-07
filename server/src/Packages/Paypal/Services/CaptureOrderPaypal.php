92b2b631de86dc9d870186bb564293007cf2df8f
Alexeia <alexeiaw@gmail.com>
2020-07-07 12:05:26 +0200

    add debug to paypal

--- server/src/Packages/Paypal/Services/CaptureOrderPaypal.php
+++ server/src/Packages/Paypal/Services/CaptureOrderPaypal.php
@@ -2,6 +2,7 @@
 
 namespace  Gernzy\Server\Packages\Paypal\Services;
 
+use Illuminate\Support\Facades\Log;
 use PayPalCheckoutSdk\Orders\OrdersCaptureRequest;
 
 class CaptureOrderPaypal
@@ -25,21 +26,21 @@ class CaptureOrderPaypal
         $response = $client->execute($request);
         // 4. Save the capture ID to your database. Implement logic to save capture to your database for future reference.
         if ($debug) {
-            print "Status Code: {$response->statusCode}\n";
-            print "Status: {$response->result->status}\n";
-            print "Order ID: {$response->result->id}\n";
-            print "Links:\n";
+            Log::debug("Status Code: {$response->statusCode}\n", ['package' => "Paypal"]);
+            Log::debug("Status: {$response->result->status}\n", ['package' => "Paypal"]);
+            Log::debug("Order ID: {$response->result->id}\n", ['package' => "Paypal"]);
+            Log::debug("Links:\n", ['package' => "Paypal"]);
             foreach ($response->result->links as $link) {
-                print "\t{$link->rel}: {$link->href}\tCall Type: {$link->method}\n";
+                Log::debug("\t{$link->rel}: {$link->href}\tCall Type: {$link->method}\n", ['package' => "Paypal"]);
             }
-            print "Capture Ids:\n";
+            Log::debug("Capture Ids:\n", ['package' => "Paypal"]);
             foreach ($response->result->purchase_units as $purchase_unit) {
                 foreach ($purchase_unit->payments->captures as $capture) {
-                    print "\t{$capture->id}";
+                    Log::debug("\t{$capture->id}", ['package' => "Paypal"]);
                 }
             }
             // To print the whole response body, uncomment the following line
-            // echo json_encode($response->result, JSON_PRETTY_PRINT);
+            Log::debug(json_encode($response->result, JSON_PRETTY_PRINT), ['package' => "Paypal"]);
         }
 
         return $response;
