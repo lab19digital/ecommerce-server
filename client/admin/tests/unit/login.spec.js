8cc7f50ed7c6d5bb076a6f3edd0241301d63aa21
Alexeia <alexeiaw@gmail.com>
2020-08-11 15:26:01 +0200

    all tests passing, fixes after version updates and deprecations

--- client/admin/tests/unit/login.spec.js
+++ client/admin/tests/unit/login.spec.js
@@ -1,64 +1,76 @@
-import Vue from 'vue'
-import { mount } from "@vue/test-utils"
-import Login from "../../src/components/login.vue"
-import createStore from "../../src/store/store"
-import { createLocalVue, createApolloTestProvider, makeGetInitialState } from './helper'
+import Vue from "vue";
+import { mount } from "@vue/test-utils";
+import Login from "../../src/components/login.vue";
+import createStore from "../../src/store/store";
+import {
+  createLocalVue,
+  createApolloTestProvider,
+  makeGetInitialState
+} from "./helper";
 
-const localVue = createLocalVue()
-const store = createStore()
-const apolloProvider = createApolloTestProvider()
-const attachToDocument = true
-const getInitialState = makeGetInitialState( store );
+const localVue = createLocalVue();
+const store = createStore();
+const apolloProvider = createApolloTestProvider();
+const attachToDocument = true;
+const getInitialState = makeGetInitialState(store);
 
 describe("Login", () => {
-
   beforeEach(() => {
-    store.replaceState( getInitialState() )
-  })
+    store.replaceState(getInitialState());
+  });
 
   test("should render content correctly", () => {
     const wrapper = mount(Login, {
       apolloProvider
-    })
-    expect(wrapper.find('label[for="email"]').text()).toEqual("Username")
-  })
+    });
+    expect(wrapper.find('label[for="email"]').text()).toEqual("Username");
+  });
 
-  test('can handle successful login when submit button is clicked', async function( done ){
+  test("can handle successful login when submit button is clicked", async function(done) {
+    const elem = document.createElement("div");
+    if (document.body) {
+      document.body.appendChild(elem);
+    }
+
     const wrapper = mount(Login, {
       store,
       localVue,
       apolloProvider,
-      attachToDocument
-    })
-    const email = wrapper.find('#email')
-    const passw = wrapper.find('#password')
-    email.setValue("luke@example.com")
-    passw.setValue("password")
-    wrapper.find('button[type="submit"]').trigger('click')
+      attachTo: elem
+    });
+    const email = wrapper.find("#email");
+    const passw = wrapper.find("#password");
+    email.setValue("luke@example.com");
+    passw.setValue("password");
+    wrapper.find('button[type="submit"]').trigger("click");
 
-    await Vue.nextTick()
-    expect(store.state.session.has_active_session).toEqual(true)
-    wrapper.destroy()
-    done()
-  })
+    await Vue.nextTick();
+    expect(store.state.session.has_active_session).toEqual(true);
+    wrapper.destroy();
+    done();
+  });
 
-  test('can handle failed login when submit button is clicked', async function( done ){
+  test("can handle failed login when submit button is clicked", async function(done) {
+    const elem = document.createElement("div");
+    if (document.body) {
+      document.body.appendChild(elem);
+    }
+
     const wrapper = mount(Login, {
       store,
       localVue,
       apolloProvider,
-      attachToDocument,
-    })
-    const email = wrapper.find('#email')
-    const passw = wrapper.find('#password')
-    email.setValue('')
-    passw.setValue('')
-    wrapper.find('button[type="submit"]').trigger('click')
+      attachTo: elem
+    });
+    const email = wrapper.find("#email");
+    const passw = wrapper.find("#password");
+    email.setValue("");
+    passw.setValue("");
+    wrapper.find('button[type="submit"]').trigger("click");
 
-    await Vue.nextTick()
-    expect(store.state.session.has_active_session).toEqual(false)
-    wrapper.destroy()
-    done()
-  })
-
-})
+    await Vue.nextTick();
+    expect(store.state.session.has_active_session).toEqual(false);
+    wrapper.destroy();
+    done();
+  });
+});
--- client/admin/tests/unit/router.spec.js
+++ client/admin/tests/unit/router.spec.js
@@ -1,18 +1,18 @@
-import { shallowMount, mount, createLocalVue } from "@vue/test-utils"
-import VueRouter from "vue-router"
-import createRouter from '../../src/router';
-import App from '../../src/components/App.vue';
-import Login from "@/components/Login.vue"
+import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
+import VueRouter from "vue-router";
+import App from "../../src/components/App.vue";
+import Login from "@/components/Login.vue";
+import routes from "@/routes.js";
 
-const localVue = createLocalVue()
-localVue.use(VueRouter)
+const localVue = createLocalVue();
+localVue.use(VueRouter);
 
 describe("Router", () => {
-  test("should mount the router and see login", () => {
-    const router = createRouter();
-    const wrapper = mount(App, { localVue, router })
-    router.push('/login');
-    expect(wrapper.find(Login).exists()).toBe(true)
-
+  test("should mount the router and see login", async () => {
+    const router = new VueRouter({ routes });
+    const wrapper = mount(App, { localVue, router });
+    router.push("/login");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.findComponent(Login).exists()).toBe(true);
   });
 });
