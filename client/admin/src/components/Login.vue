e60b6d355e0329571027b617c2a9fba4ca3d23e1
Alexeia <alexeiaw@gmail.com>
2020-08-12 16:51:16 +0200

    login form feedback to user

--- client/admin/src/components/Login.vue
+++ client/admin/src/components/Login.vue
@@ -53,7 +53,43 @@
     <p class="text-center text-gray-500 text-xs">
       &copy;2020 Gernzy. All rights reserved.
     </p>
-    <div>Name: {{ name }}</div>
+
+    <!-- Success -->
+    <div
+      v-if="activeSesssion"
+      class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+      role="alert"
+    >
+      <div class="flex">
+        <div class="py-1">
+          <svg
+            class="fill-current h-6 w-6 text-teal-500 mr-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"
+            />
+          </svg>
+        </div>
+        <div>
+          <p class="font-bold">You successfully logged in</p>
+          <p class="text-sm">Welcome to the admin panel {{ name }}</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Failed -->
+    <div v-if="errors.length" role="alert">
+      <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+        An error occurred
+      </div>
+      <div
+        class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700"
+      >
+        <p v-for="(error, key) in errors" :key="key">{{ error }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -70,6 +106,7 @@ export default {
   }),
   computed: mapState({
     name: (state) => state.session.name,
+    activeSesssion: (state) => state.session.has_active_session,
   }),
   methods: {
     checkForm: async function (event) {
