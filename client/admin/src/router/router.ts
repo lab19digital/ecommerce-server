f2ce5b975a6efb2cc5da3a48e83a25049fd218e3
Alexeia <alexeiaw@gmail.com>
2020-09-02 09:07:51 +0200

    - improved gauding routes with meta to say if route is gaurded

--- client/admin/src/router/router.ts
+++ client/admin/src/router/router.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import routes from "./routes";
+import store from "@/store/store";
 
 Vue.use(VueRouter);
 
@@ -8,6 +9,24 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  // Check if the user is logged i
+  const isUserLoggedIn = store.getters.isAuthenticated;
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!isUserLoggedIn) {
+      // store.dispatch("logOut");
+      next({
+        path: "/login",
+        query: { redirectFrom: to.fullPath },
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
--- client/admin/src/router/routes.ts
+++ client/admin/src/router/routes.ts
@@ -1,39 +1,11 @@
 import Login from "@/views/Login.vue";
 import Dashboard from "@/views/Dashboard.vue";
-import store from "@/store/store";
 
 export default [
   {
     path: "/dashboard",
     component: Dashboard,
-    beforeEnter: (to: any, from: any, next: any) => {
-      /**
-       * Persist the session data from vuex mutation for use
-       * */
-
-      let is_admin = 0;
-      let token = localStorage.getItem("apollo-token");
-
-      try {
-        if (localStorage.getItem("session") !== null) {
-          let session = JSON.parse(localStorage.getItem("session") || "");
-          is_admin = session.user.is_admin;
-        }
-      } catch (error) {
-        console.log(error);
-      }
-
-      if (token === null || is_admin == 0) {
-        next({
-          path: "/login", // back to safety
-          query: {
-            redirectFrom: to.fullPath,
-          },
-        });
-      } else {
-        next();
-      }
-    },
+    meta: { requiresAuth: true },
   },
   {
     path: "/login",
