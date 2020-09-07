a8b682b3d0d81846b6da57192f68cae6565b3ec0
Alexeia <alexeiaw@gmail.com>
2020-09-07 14:45:11 +0200

    - vue appolo auth fix, duplicate bearer token - me graphql query

--- client/admin/src/graphql/queries.ts
+++ client/admin/src/graphql/queries.ts
@@ -1,16 +1,9 @@
 import gql from "graphql-tag";
 
-// export const LOGGED_IN_USER = gql`
-//   query {
-//     me {
-//       name
-//     }
-//   }
-// `;
-
 export const LOGGED_IN_USER = gql`
   query {
-    user(id: 1) {
+    me {
+      id
       name
       email
       is_admin
