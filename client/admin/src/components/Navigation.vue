d08eb4b78d641ef48472b96c0794b283e8f6087c
Alexeia <alexeiaw@gmail.com>
2020-09-07 12:33:14 +0200

    - improve navigation login and logout

--- client/admin/src/components/Navigation.vue
+++ client/admin/src/components/Navigation.vue
@@ -34,28 +34,7 @@
           <router-link
             class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
             to="/"
-            >Root</router-link
-          >
-          <router-link
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-            to="/login"
-            v-if="!isAuthenticated && !isAdmin"
-            >Login</router-link
-          >
-
-          <!-- <router-link
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-            to="#/"
-            v-if="isAuthenticated && isAdmin"
-            @click="logOutClick"
-            >Logout</router-link
-          > -->
-          <a
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-            v-if="isAuthenticated && isAdmin"
-            @click="logOutClick"
-            href="#/"
-            >Logout</a
+            >Home</router-link
           >
 
           <router-link
@@ -68,7 +47,16 @@
           <router-link
             class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
             to="/login"
+            v-if="!isAuthenticated && !isAdmin"
             >Login</router-link
+          >
+
+          <a
+            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            v-if="isAuthenticated && isAdmin"
+            @click="logOutClick"
+            href="#/"
+            >Logout</a
           >
         </div>
       </div>
