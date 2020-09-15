bd926e11f19ab798aa2f6a4cee4a6eadf6a30aff
Alexeia <alexeiaw@gmail.com>
2020-09-15 14:51:53 +0200

    -  test table prev/next range constraints for current page and has more pages

--- client/admin/tests/unit/table.spec.ts
+++ client/admin/tests/unit/table.spec.ts
@@ -30,7 +30,7 @@ describe("Table", () => {
   });
 
   test("can handle successful next button is clicked", async function (done) {
-    const wrapper = shallowMount(Table, {
+    const wrapper: any = shallowMount(Table, {
       store,
       localVue,
       apolloProvider,
@@ -38,17 +38,13 @@ describe("Table", () => {
 
     await flushPromises();
 
-    // @ts-ignore
     const originalCurrentPage: any = wrapper.vm.paginatorInfo.currentPage;
     expect(originalCurrentPage == 0).toBe(false);
-    // @ts-ignore
     wrapper.vm.paginatorInfo.currentPage = 1;
-    // @ts-ignore
     wrapper.vm.paginatorInfo.hasMorePages = true;
 
     wrapper.find("#pnext").trigger("click");
     await flushPromises();
-    // @ts-ignore
     const newCurrentPage: any = wrapper.vm.paginatorInfo.currentPage;
     expect(originalCurrentPage == newCurrentPage).toBe(false);
     expect(wrapper.findComponent(Table).exists()).toBe(true);
@@ -57,8 +53,8 @@ describe("Table", () => {
     done();
   });
 
-  test("can handle successful paginator input change", async function (done) {
-    const wrapper = shallowMount(Table, {
+  test("can handle successful prev button is clicked", async function (done) {
+    const wrapper: any = shallowMount(Table, {
       store,
       localVue,
       apolloProvider,
@@ -66,20 +62,66 @@ describe("Table", () => {
 
     await flushPromises();
 
-    // @ts-ignore
+    const originalCurrentPage: any = wrapper.vm.paginatorInfo.currentPage;
+    expect(originalCurrentPage == 0).toBe(false);
+    wrapper.vm.paginatorInfo.currentPage = 2;
+    wrapper.vm.paginatorInfo.hasMorePages = true;
+
+    wrapper.find("#pprev").trigger("click");
+    await flushPromises();
+    const newCurrentPage: any = wrapper.vm.paginatorInfo.currentPage;
+    expect(originalCurrentPage == newCurrentPage).toBe(false);
+    expect(wrapper.findComponent(Table).exists()).toBe(true);
+
+    wrapper.destroy();
+    done();
+  });
+
+  test("can handle failed prev/next button is clicked", async function (done) {
+    const wrapper: any = shallowMount(Table, {
+      store,
+      localVue,
+      apolloProvider,
+    });
+
+    await flushPromises();
+
+    wrapper.vm.paginatorInfo.hasMorePages = false;
+
+    wrapper.find("#pnext").trigger("click");
+    await flushPromises();
+    expect(wrapper.vm.errors.length > 0).toBe(true);
+
+    wrapper.vm.resetErrors();
+
+    wrapper.vm.paginatorInfo.currentPage = 0;
+    wrapper.find("#pprev").trigger("click");
+    await flushPromises();
+    expect(wrapper.vm.errors.length > 0).toBe(true);
+
+    wrapper.destroy();
+    done();
+  });
+
+  test("can handle successful paginator input change", async function (done) {
+    const wrapper: any = shallowMount(Table, {
+      store,
+      localVue,
+      apolloProvider,
+    });
+
+    await flushPromises();
+
     const originalDisplayValues = Object.keys(wrapper.vm.productsDisplay[0]);
-    // @ts-ignore
     const attribute = wrapper.vm.productAttributes[5];
 
     wrapper.find(`#${attribute}`).trigger("click");
 
     const checkBox = wrapper.find(`#${attribute}`);
-    // @ts-ignore
     checkBox.element.selected = false;
     checkBox.trigger("change");
     await Vue.nextTick();
 
-    // @ts-ignore
     const mutatedDisplayValues = Object.keys(wrapper.vm.productsDisplay[0]);
 
     expect(originalDisplayValues.length == mutatedDisplayValues.length).toBe(
