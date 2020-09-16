5c72dd42ccb1225eb37374b4e4ae42297037ed6e
Alexeia <alexeiaw@gmail.com>
2020-09-16 10:06:49 +0200

    - fix for : This test fails with  Error in render: "TypeError: Cannot read property '_modulesNamespaceMap' of undefined" - not sure why this way works, but it does!  :)

--- client/admin/tests/unit/router.spec.ts
+++ client/admin/tests/unit/router.spec.ts
@@ -1,21 +1,37 @@
-import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import VueRouter from "vue-router";
 import App from "@/components/App.vue";
 import Login from "@/views/Login.vue";
 import routes from "@/router/routes.ts";
+import store from "@/store/store";
+import {
+  createLocalVue,
+  createApolloTestProvider,
+  makeGetInitialState,
+} from "./helper";
 
 const localVue = createLocalVue();
 localVue.use(VueRouter);
+const apolloProvider = createApolloTestProvider();
+const getInitialState = makeGetInitialState(store);
 
 describe("Router", () => {
+  beforeEach(() => {
+    store.replaceState(getInitialState());
+  });
+
   test("should mount the router and see login", async () => {
-    // TODO: This test fails with  Error in render: "TypeError: Cannot read property '_modulesNamespaceMap' of undefined"
-    // vuex-class, vuex, localVue packages need fix
-    // const router = new VueRouter({ routes });
-    // const wrapper = mount(App, { localVue, router });
-    // router.push("/login");
-    // await wrapper.vm.$nextTick();
-    // expect(wrapper.findComponent(Login).exists()).toBe(true);
-    expect(true).toBe(true);
+    const router = new VueRouter({ routes });
+
+    const wrapper = mount(App, {
+      store,
+      router,
+      localVue,
+      apolloProvider,
+    });
+
+    router.push("/login");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.findComponent(Login).exists()).toBe(true);
   });
 });
