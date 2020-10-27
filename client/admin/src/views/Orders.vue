9d4805c3ce118835ce0bcaf0d1eb888f82c46273
Alexeia <alexeiaw@gmail.com>
2020-10-27 16:58:46 +0200

    in the orders component, using regex and going through table columns to try and find the links for the id column, then making them into links and using innerHTML to update the td html This may not be the best solution, but the table component just renders data not innerhtml, and doesn't know if the data is a link

--- client/admin/src/views/Orders.vue
+++ client/admin/src/views/Orders.vue
@@ -19,7 +19,9 @@
       />
     </div>
 
-    <Table :columns="tableColums" :rows="ordersDisplay" />
+    <div id="ordersTable">
+      <Table :columns="tableColums" :rows="ordersDisplay" />
+    </div>
 
     <!-- Settings -->
     <button
@@ -202,6 +204,27 @@ export default class Orders extends Vue {
           // Populate column data
           this.ordersDisplay = filterArray(this.tableColums, this.orders, {
             url: window.location.href,
+          });
+
+          this.$nextTick(function () {
+            var tds = document.querySelectorAll("#ordersTable td");
+            //eslint-disable-next-line
+            let re: any = tds.forEach((element) => {
+              // eslint-disable-next-line
+              let re: any = /\/orders\//;
+              var res: any = re.exec(element.innerHTML);
+
+              if (res) {
+                // @ts-ignore
+                let link = element.childNodes[0].innerHTML;
+                element.innerHTML =
+                  `<a class="text-teal-500 underline" href="${link}" target="_blank">` +
+                  link +
+                  "</b>";
+              } else {
+                return false;
+              }
+            });
           });
 
           return Promise.resolve();
