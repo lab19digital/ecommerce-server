86c360e6b09d106fdeb43a7db6cd10cedf4fc402
Alexeia <alexeiaw@gmail.com>
2020-12-01 08:50:17 +0200

    move the product types to the product interface so that it can be reusable

--- client/admin/src/types/product.ts
+++ client/admin/src/types/product.ts
@@ -1,3 +1,22 @@
 export interface Product {
-  title?: string;
+  id: number;
+  title: string;
+  price_cents: string;
+  price_currency: string;
+  short_description: string;
+  long_description: string;
+  meta: [{ key: string; value: string }];
+  prices: [{ currency: string; value: number }];
+  images: [{ url: string; name: string; type: string }];
+  sizes: [{ size: number }];
+  tags: [{ name: string }];
+  categories: [{ title: string }];
+  dimensions: [];
+  weight: [];
+  fixedPrices: [
+    {
+      country_code: string;
+      price: number;
+    }
+  ];
 }
