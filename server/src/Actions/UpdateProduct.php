b6fa70dbba1ac5a1a0b6c31a9c970fbc902ad028
Alexeia <alexeiaw@gmail.com>
2020-12-02 10:53:44 +0200

    for all incoming products, if the id is defined then update the existing image, otherwise create the new image and associate TODO: first detach images then re-attach for later

--- server/src/Actions/UpdateProduct.php
+++ server/src/Actions/UpdateProduct.php
@@ -4,7 +4,9 @@ namespace Gernzy\Server\Actions;
 
 use \App;
 use Gernzy\Server\Actions\Helpers\Attributes;
+use Gernzy\Server\Models\Image;
 use Gernzy\Server\Models\Product;
+use Illuminate\Support\Facades\Log;
 
 class UpdateProduct
 {
@@ -45,6 +47,38 @@ class UpdateProduct
         $product->attributes()->createMany(
             $attributes->toArray()
         );
+
+        // Update the images
+        // $product->images()->delete();
+        $images = json_decode($args["images"]);
+
+        // Log::debug($images);
+
+
+        // $imagesNew = [];
+
+        foreach ($images as $image) {
+            if ($image->id > 0) {
+                $imageFind = Image::find($image->id);
+                $imageFind->name = $image->name;
+                $imageFind->url = $image->url;
+                $imageFind->type = $image->type;
+                $imageFind->save();
+            } else {
+                $imageNew = new Image(["name" => $image->name, "url" => $image->url, "type" => $image->type]);
+                // $imageNew->save();
+                $product->images()->save($imageNew);
+            }
+
+            // $imageNew = new Image(["name" => $image->name, "url" => $image->url, "type" => $image->type]);
+            // if ($image->id > 0) {
+            //     $imageNew->id = $image->id;
+            // }
+            // array_push($imagesNew, $imageNew);
+        }
+
+        // $images = $product->images()->saveMany($imagesNew);
+
 
 
         // Product details
